dcec0dc1cf627d1662636e33e904ac1efb0251a1
vhfw <vanhalenisagod@gmail.com>
2021-03-01 21:31:11 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ sudo docker run -it -e DISPLAY=docker.host.internal:0.0 -v /tmp/X11-unix:/tmp/.X
 	--ipc="host" \
 	--device=/dev/snd:/dev/snd \
 	--device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/dev/nvidia0 \
-	-v"C:\\users\\ryan\\Desktop\\GamingDocker":/root/gaming
+	-v`pwd`:/root/gaming
 	--net=host \
 	--name="steam_test" \
 	steam
